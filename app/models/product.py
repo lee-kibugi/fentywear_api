797438140c1b7bc03508e87d339b3a8be48325cc
nebstarmalala <nebstartrizzle@gmail.com>
2024-03-02 08:24:57 +0300

    add image url col

--- app/models/product.py
+++ app/models/product.py
@@ -7,5 +7,6 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     stock_quantity = db.Column(db.Integer, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    image_url = db.Column(db.String(255)) 
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)
