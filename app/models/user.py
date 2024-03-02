510c4ed37966cd9192274068bfce3c88a55d846e
nebstarmalala <nebstartrizzle@gmail.com>
2024-03-02 17:00:19 +0300

    add support for flask mixin

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,8 @@
 from .. import db
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
