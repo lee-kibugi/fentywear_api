926efaab4ace50c0689a011619d8f6622ebd5276
nebstarmalala <nebstartrizzle@gmail.com>
2024-03-02 16:59:47 +0300

    create login and logout using jwt access tokens

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
 from flask_login import LoginManager, login_user, logout_user, login_required
 from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from ..models import User
 from .. import db, login_manager
 
@@ -13,22 +15,18 @@ def load_user(user_id):
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
+    if not data or not data.get('email') or not data.get('password'):
+        return jsonify({'error': 'Email and password are required'}), 400
     
-    if not data or not data.get('username') or not data.get('password'):
-        return jsonify({'error': 'Username and password are required'}), 400
-    
-    username = data['username']
-    password = data['password']
-    user = User.query.filter_by(username=username).first()
-    
-    if user and check_password_hash(user.password_hash, password):
-        login_user(user)
-        return jsonify({'message': 'Login successful'}), 200
+    user = User.query.filter_by(email=data['email']).first()
+    if user and check_password_hash(user.password_hash, data['password']):
+        access_token = create_access_token(identity=user.id)
+        return jsonify(access_token=access_token), 200
     else:
-        return jsonify({'error': 'Invalid username or password'}), 401
+        return jsonify({'error': 'Invalid email or password'}), 401
 
 @auth_bp.route('/logout', methods=['POST'])
-@login_required
+@jwt_required()
 def logout():
     logout_user()
     return jsonify({'message': 'Logout successful'}), 200
