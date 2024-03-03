e7bde6dc6e7fc960eac85821de51c0db619feeb4
nebstarmalala <nebstartrizzle@gmail.com>
2024-03-03 09:35:56 +0300

    remove

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,13 +1,13 @@
-from flask import Flask
-from .user_routes import user_bp
-from .category_routes import category_bp
-from .product_routes import product_bp
-from .review_routes import review_bp
-from .authentication import auth_bp
+# from flask import Flask
+# from .user_routes import user_bp
+# from .category_routes import category_bp
+# from .product_routes import product_bp
+# from .review_routes import review_bp
+# from .authentication import auth_bp
 
-def register_blueprints(app: Flask):
-    app.register_blueprint(user_bp, url_prefix='/users')
-    app.register_blueprint(category_bp, url_prefix='/categories')
-    app.register_blueprint(product_bp, url_prefix='/products')
-    app.register_blueprint(review_bp, url_prefix='/reviews')
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+# def register_blueprints(app: Flask):
+#     app.register_blueprint(user_bp, url_prefix='/users')
+#     app.register_blueprint(category_bp, url_prefix='/categories')
+#     app.register_blueprint(product_bp, url_prefix='/products')
+#     app.register_blueprint(review_bp, url_prefix='/reviews')
+#     app.register_blueprint(auth_bp, url_prefix='/auth')
